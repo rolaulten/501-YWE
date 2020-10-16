ecb1b471ba8890043b0ae2783d456a4830fc1644
rolaulten <49468451+rolaulten@users.noreply.github.com>
2020-10-16 13:58:01 -0700

    Update RunMe.bat

--- RunMe.bat
+++ RunMe.bat
@@ -1,2 +1,2 @@
-start /wait powershell.exe -executionpolicy bypass -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/rolaulten/501-YWE/main/GetComputerInfo.ps1P -Outfile .\GetInfo.ps1"
-powershell.exe executionpolicy bypass -Command "powershell.exe '.\GetInfo.ps1' -Verb runAs"
+start /wait powershell.exe -executionpolicy bypass -noprofile -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/rolaulten/501-YWE/main/GetComputerInfo.ps1P -Outfile .\GetInfo.ps1"
+powershell.exe -noprofile -executionpolicy bypass -Command "powershell.exe '.\GetInfo.ps1' -Verb runAs"
