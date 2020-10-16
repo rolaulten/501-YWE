5746b7d8b6d2b51a6bcc97a8dbb6bffdd80cfc43
rolaulten <49468451+rolaulten@users.noreply.github.com>
2020-10-16 14:08:17 -0700

    Delete RunMe.bat

--- RunMe.bat
+++
@@ -1,2 +0,0 @@
-start /wait powershell.exe -executionpolicy bypass -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/rolaulten/501-YWE/main/GetComputerInfo.ps1 -Outfile .\GetInfo.ps1"
-powershell.exe -noprofile -executionpolicy bypass -Command "powershell.exe '.\GetInfo.ps1' -Verb runAs"
