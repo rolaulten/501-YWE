7dca1ff423e41d0c2c3810e21c38a2bceb837c7d
rolaulten <49468451+rolaulten@users.noreply.github.com>
2020-10-16 12:58:14 -0700

    Updated path to github raw

--- RunMe.bat
+++ RunMe.bat
@@ -1,2 +1,2 @@
-start /wait powershell.exe -executionpolicy bypass -Command "Invoke-WebRequest -Uri https://501commons-my.sharepoint.com/:u:/g/personal/dorian_501commons_org/EeL_uxRTJj5MphyVMuOhlsIBL1OS2f0r8UTapjVmsTlh5g?e=VNrRVP -Outfile .\Test.ps1"
-powershell.exe executionpolicy bypass -Command "powershell.exe '.\Test.ps1' -Verb runAs"
+start /wait powershell.exe -executionpolicy bypass -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/rolaulten/501-YWE/main/GetComputerInfo.ps1P -Outfile .\GetInfo.ps1"
+powershell.exe executionpolicy bypass -Command "powershell.exe '.\GetInfo.ps1' -Verb runAs"
