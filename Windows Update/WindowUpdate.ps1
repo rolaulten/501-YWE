170da30814f72e943bf3ec675724221f53e740aa
Dorian Goettler <Dorian@501commons.org>
2020-11-19 10:08:25 -0800

    saving

--- Windows Update/WindowUpdate.ps1
+++ Windows Update/WindowUpdate.ps1
@@ -18,7 +18,7 @@ $WinTrigger = New-ScheduledTaskTrigger -At 3:00PM -Weekly -DaysOfWeek Wednesday
 $WinAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command {Install-WindowsUpdate -MicrosoftUpdate -AcceptAll}"
 Register-ScheduledTask -Action $WinAction -Trigger $WinTrigger -TaskName $WinTaskname
 
-
+#Install Chrome
 #Install office
 Start-Process .\office\setup.exe -wait
 
